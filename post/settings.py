623012adc9a0712a6e746168a7119cd60b6c2b2b
dexbob <mimacro@naver.com>
2024-11-04 22:33:04 +0900

    설정파일에 ALLOWED_HOSTS 및 CORS_ALLOWED_ORIGINS 수정

--- post/settings.py
+++ post/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&*)$l9e7c2l@s5hpyr@tpktx#r8l8_$@57u1lw%5_7q*m(%19^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://django-deploy-xkhx.onrender.com/']
 
 
 # Application definition
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [  
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'https://react-deploy-two-green.vercel.app/',
 ]
 
 ROOT_URLCONF = "post.urls"
