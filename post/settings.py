b848fd9f2672dc2b18d839f93232c1928e73473b
dexbob <mimacro@naver.com>
2024-11-04 22:45:43 +0900

    배포시 빌드에러 대응

--- post/settings.py
+++ post/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [  
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    'https://react-deploy-two-green.vercel.app/',
+    'https://react-deploy-two-green.vercel.app',
 ]
 
 ROOT_URLCONF = "post.urls"
